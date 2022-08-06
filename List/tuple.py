ffdcbb47528d1cc7333efff0798eb04e9420b005
AlexoAnimaIgnis <ballester.alexander@gmail.com>
2022-08-06 22:39:48 +0800

    tuples are immutable list

---
+++ List/tuple.py
@@ -0,0 +1,8 @@
+# Tuples are immutable list.
+
+# Tuple is just like a list but you use parentheses instead
+# instead of brackets
+
+dimensions = (200, 150)
+print(dimensions[0])
+print(dimensions[1])
