b3d9cf31ad799f0152984625055dce60865e8b4f
AlexoAnimaIgnis <ballester.alexander@gmail.com>
2022-08-06 22:50:15 +0800

    adding dynamic values to a dictionary

---
+++ Dictionaries/alien.py
@@ -0,0 +1,17 @@
+alien_0 = {'color': 'green', 'points':5}
+print(alien_0['color'])
+print(alien_0['points'])
+
+# dictionary is a collection of key-value pairs.
+# Each key is connected to a value and you can use
+# key to access the value associated with that key.
+
+
+# When you provide a key, python returns the associated value
+
+# Dictionaries are dynamic structures and you can add
+# new key-value pairs to a dictionary any time.
+print(alien_0)
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+print(alien_0)
