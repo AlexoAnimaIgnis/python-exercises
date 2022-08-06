a3ba0cb658eb5a7efe8910044d4d83ac8084da54
AlexoAnimaIgnis <ballester.alexander@gmail.com>
2022-08-06 21:46:21 +0800

    Strings and Number exercises

---
+++ List/bicycles.py
@@ -0,0 +1,9 @@
+bicycles = ['trek', 'cannondale','redline', 'specialized']
+print(bicycles)
+
+# Accessing Elements in a List
+# Lists are ordered collections so you can access eleemnt in a list by 
+# telling python the position or index
+print(bicycles[0].title())
+message = f"My first bicycle was a {bicycles[0].title()}."
+print(message)
