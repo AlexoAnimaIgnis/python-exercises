c3ec00801199b4a23934c51c54e60af299207a76
AlexoAnimaIgnis <ballester.alexander@gmail.com>
2022-08-06 22:34:09 +0800

    ranged variation

---
+++ List/even_numbers.py
@@ -0,0 +1,6 @@
+even_numbers = list(range(2,11,2))
+# list(range(2,11,2)) 
+# first parameter start
+# third parameter means add to to the value
+# second parameter is the end value
+print(even_numbers)
